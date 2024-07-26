3ad0e4faf62811fd4a61093a1281d94269947481
Saurabh Band <saurabhba@cybage.com>
2024-07-26 21:17:43 +0530

    UI changes for shadow

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -13,16 +13,24 @@ public partial class HomePage : ContentPage
 		//border.Shadow.Brush = Colors.White;
 		//border.Background = Colors.White;
 		//border.Shadow = new Shadow { Brush = Colors.Red, Offset = new Point(0,15), Opacity = (float)0.8 };
-		var border = sender as Border;
-		await border.ScaleTo(0.95, 75, Easing.BounceOut);
-		await border.ScaleTo(1.0, 75, Easing.BounceIn);
+		var borders = sender as Border;
+		borders.Shadow = new Shadow { Brush = Colors.Transparent };
+		await borders.ScaleTo(0.95, 75, Easing.BounceOut);
+		await borders.ScaleTo(1.0, 75, Easing.BounceIn);
+		borders.Shadow = new Shadow { Brush = Color.FromHex("#570099") , Offset = new Point(0,15), Opacity = (float)0.15 };
 	}
 
     private async void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
     {
-		var border = sender as Border;
-		await border.ScaleTo(0.95, 75, Easing.BounceOut);
-		await border.ScaleTo(1.0, 75, Easing.BounceIn);
-		
+		var borders = sender as Border;
+		borders.Shadow = new Shadow { Brush = Colors.Transparent };
+		await borders.ScaleTo(0.95, 75, Easing.BounceOut);
+		await borders.ScaleTo(1.0, 75, Easing.BounceIn);
+		borders.Shadow = new Shadow { Brush = Color.FromHex("#570099"), Offset = new Point(0, 15), Opacity = (float)0.15 };
+
 	}
 }
+//Brush="#570099"
+//                        Offset="0,15"
+//                        Radius="5" 
+//                        Opacity="0.15"
